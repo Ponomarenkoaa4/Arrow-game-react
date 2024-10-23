7f4bb917c6b3f54c05b298a618bd8fc8f07c84c0
Anna Ponomarenko <ponomarenko0904@yandex.ru>
2024-10-23 16:38:42 +0600

    first commit

---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,23 @@
+import { Button as MaterialButton } from "@mui/material"
+import type { ButtonProps as MaterialButtonProps } from "@mui/material"
+import cn from "classnames"
+import styles from "./Button.module.css"
+
+export interface IButtonProps extends MaterialButtonProps {}
+
+const Button: React.FC<IButtonProps> = (props) => {
+  const { children, className = "" } = props
+
+  return (
+    <MaterialButton
+      variant="contained"
+      size="small"
+      {...props}
+      className={cn(styles.button, className)}
+    >
+      {children}
+    </MaterialButton>
+  )
+}
+
+export default Button
---
+++ src/components/UI/TypographyHeader/TypographyHeader.tsx
@@ -0,0 +1,23 @@
+import type { TypographyProps as MaterialTypographyProps } from "@mui/material"
+import { Typography as MaterialTypography } from "@mui/material"
+import cn from "classnames"
+
+import styles from "./TypographyHeader.module.css"
+
+export interface ITypographyHeaderProps extends MaterialTypographyProps {}
+
+const TypographyHeader: React.FC<ITypographyHeaderProps> = (props) => {
+  const { children, className = "" } = props
+
+  return (
+    <MaterialTypography
+      variant="h3"
+      {...props}
+      className={cn(styles.text, className)}
+    >
+      {children}
+    </MaterialTypography>
+  )
+}
+
+export default TypographyHeader
