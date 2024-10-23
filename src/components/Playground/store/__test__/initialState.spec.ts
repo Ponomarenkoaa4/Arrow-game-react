d1e4fe04886eec1b427c5578c04595a5fc7d5874
Anna Ponomarenko <ponomarenko0904@yandex.ru>
2024-10-23 16:41:02 +0600

    gh-pages

--- src/components/Playground/store/__test__/initialState.spec.ts
+++
@@ -1,9 +0,0 @@
-import playgroundReducer, { initialState } from "../slices"
-
-describe("initialState", () => {
-  it("check initial state", () => {
-    const state = playgroundReducer(undefined, { type: "unknown" })
-
-    expect(state).toEqual(initialState)
-  })
-})
